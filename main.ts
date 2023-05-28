60f0d54522f1c86c2b267ca68c01ecd7aa4ad6b5
cyuyuc <130678477+cyuyuc@users.noreply.github.com>
2023-05-28 23:25:45 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,30 +7,33 @@ robotbit.Servo(robotbit.Servos.S1, 180)
 basic.pause(500)
 robotbit.Servo(robotbit.Servos.S1, 0)
 basic.forever(function () {
-    radio.sendNumber(Environment.ReadDust(DigitalPin.P16, AnalogPin.P1))
-})
-basic.forever(function () {
     distance = sonar.ping(
     DigitalPin.P2,
     DigitalPin.P12,
     PingUnit.Centimeters
     )
-    if (distance <= 70) {
+    if (distance <= 60) {
         radio.sendValue("Unavailable", 3)
         availability = 2
         sos += 0.2
-    } else if (distance >= 70 && availability == 2) {
+    } else if (distance >= 60 && availability == 2) {
         radio.sendValue("Flushing", 2)
         robotbit.Servo(robotbit.Servos.S1, 180)
         availability = 1
         sos = 0
         basic.pause(1000)
         robotbit.Servo(robotbit.Servos.S1, 0)
-    } else if (distance >= 70 && availability == 1) {
+    } else if (distance >= 60) {
         radio.sendValue("Available", 1)
         sos = 0
+        availability = 1
     } else {
         radio.sendValue("Error", 4)
+    }
+    if (sos >= 60) {
+        radio.sendNumber(999)
+    } else {
+        radio.sendNumber(Environment.ReadDust(DigitalPin.P16, AnalogPin.P1))
     }
     basic.pause(1000)
 })
