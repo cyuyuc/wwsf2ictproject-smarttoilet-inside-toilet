c2358ac96f68251e7ab0d6f071d2c2ffd13d37a3
cyuyuc <130678477+cyuyuc@users.noreply.github.com>
2023-05-30 16:46:55 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,34 +3,34 @@ let distance = 0
 let availability = 1
 radio.setGroup(11)
 radio.setTransmitPower(7)
+robotbit.Servo(robotbit.Servos.S1, 90)
+basic.pause(200)
 robotbit.Servo(robotbit.Servos.S1, 180)
-basic.pause(500)
-robotbit.Servo(robotbit.Servos.S1, 0)
 basic.forever(function () {
     distance = sonar.ping(
     DigitalPin.P2,
     DigitalPin.P12,
     PingUnit.Centimeters
     )
-    if (distance <= 50) {
+    if (distance <= 90) {
         radio.sendValue("Unavailable", 3)
         availability = 2
-        sos += 0.2
-    } else if (distance >= 50 && availability == 2) {
+        sos += 1
+    } else if (distance >= 90 && availability == 2) {
         radio.sendValue("Flushing", 2)
-        robotbit.Servo(robotbit.Servos.S1, 0)
+        robotbit.Servo(robotbit.Servos.S1, 90)
         availability = 1
         sos = 0
-        basic.pause(1000)
+        basic.pause(200)
         robotbit.Servo(robotbit.Servos.S1, 180)
-    } else if (distance >= 50) {
+    } else if (distance >= 90) {
         radio.sendValue("Available", 1)
         sos = 0
         availability = 1
     } else {
         radio.sendValue("Error", 4)
     }
-    if (sos >= 3) {
+    if (sos >= 10) {
         radio.sendNumber(999)
     } else {
         radio.sendNumber(Environment.ReadDust(DigitalPin.P16, AnalogPin.P1))
